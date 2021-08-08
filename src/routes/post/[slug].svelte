28bcdaee859df0330df2cf50cbf1b3d5402b9450
braedenf <braedenleefoster@gmail.com>
2021-08-08 18:14:14 +1200

    Filtering post with dynamic route

---
+++ src/routes/post/[slug].svelte
@@ -0,0 +1,20 @@
+<script context="module">
+	export async function load({ fetch, page }) {
+		const postData = await fetch('../posts.json').then((res) => res.json());
+
+		const posts = postData.parsedPosts.result;
+		const post = posts.filter((item) => item.slug.current == page.params.slug);
+
+		return {
+			props: {
+				post
+			}
+		};
+	}
+</script>
+
+<script>
+	export let post;
+</script>
+
+<h1>{post[0].title}</h1>
